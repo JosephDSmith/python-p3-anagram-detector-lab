ad33ff8e03e4506ed4b4bb00d919f0adeb427a59
Joseph Smith <joesmith40@gmail.com>
2023-05-23 16:56:56 -0500

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possibles):
+        anagram_list = []
+        for test_word in possibles:
+            if all(letter in test_word for letter in self.word):
+                anagram_list.append(test_word)
+        return anagram_list
+            
+                
